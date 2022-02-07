6b505baa9ea0ff0ece4147f935261f17e64cf790
Rafał Rudnicki <rafalr0202@gmail.com>
2022-02-07 23:16:14 +0100

    Klon

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Umiesc mnie na GitHub'ie");
         System.out.println("Zmiana");
+        System.out.println("Modyfikacja klon");
     }
 }
